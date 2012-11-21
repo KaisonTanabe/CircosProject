542ba1ecd8e9b0f67e416fd456fdbd5f50e0a05c
scottsanderson <ssanderson@quantopian.com>
2012-11-21 18:43:47 -0500

    math major data ready for export

--- csv_to_circos.py
+++ csv_to_circos.py
@@ -1,25 +1,35 @@
-from definitions import majors
-from csv_to_mongo import read_csv, filter_by_string, read_filled_csv
+import os
+
+from definitions import majors, student_csv
+from csv_to_mongo import read_csv, filter_by_major, read_filled_csv
 from collections import defaultdict
 
-def math_double_major(input_filename, output_dirname):
+def math_double_major(output_dirname=os.getcwd(), input_filename=student_csv):
 
+    count = 0
     major_counter = defaultdict(int)
     industry_counter = defaultdict(int)
 
     reader = read_filled_csv(input_filename)
-    filtered_reader = filter_by_string(reader, "Mathematics")
+    filtered_reader = filter_by_major(reader, 'Mathematics')
 
-    for index, entry in enumerate(filtered_reader):
-        pass
-        
+    for entry in filtered_reader:
+
+        count += 1
+        if industry_counter[entry['Industry']] == '':
+            continue
+
+        industry_counter[entry['Industry']] += 1
+        for major in set((entry['Major1'], entry['Major2'], entry['Major3'])):
+            major_counter[major] += 1
+
 def count_double_majors(base_major = "Mathematics"):
     print "###############"
     print "Starting %s:" % base_major
     print "###############"
-
+    
     major_counter = defaultdict(int)
-
+    
     filtered_reader = filter_by_string(read_filled_csv(), base_major)
     
     for entry in filtered_reader:
@@ -33,7 +43,6 @@ def count_double_majors(base_major = "Mathematics"):
     print "###############"
     print "Finished %s:" % base_major
     print "###############"
-            
 
 if __name__ == "__main__":
     
--- csv_to_mongo.py
+++ csv_to_mongo.py
@@ -35,8 +35,8 @@ def fill_industries(reader):
                 yield entry   
 
 def fill_majors(reader):
+
     for entry in reader:
-        
         maj1_value = entry['Major1']
         maj2_value = entry['Major2']
         maj3_value = entry['Major3']
@@ -59,8 +59,17 @@ def match_definitions(info_str):
         for word in keywords:
             if info_str in word:
                 return industry
-        
-def filter_by_string(reader, match_string):
+
+def filter_by_major(reader, match_string):
+    for entry in reader:
+        if match_string in entry['Major1']:
+            yield entry
+        elif match_string in entry['Major2']:
+            yield entry
+        elif match_string in entry['Major3']:
+            yield entry
+
+def filter_by_any_field(reader, match_string):
     for entry in reader:
         for key, value in entry.items():
             if match_string in value:
@@ -83,7 +92,5 @@ def job_title_frequencies(reader):
             out_dict[fow] += 1
     return out_dict
     
-    
 if __name__ == "__main__":
     reader = read_filled_csv()
-    
