74ac622d2541a214714314eefe091388cf06245b
Scott Sanderson <scoutoss@gmail.com>
2013-04-04 04:31:07 -0400

    more examples

--- example.py
+++ example.py
@@ -19,6 +19,30 @@ def original_image():
 
     conf.produce_image()
 
+# Same as above, but filtering to only draw Mathematics majors.
+def original_image_filtered():
+    
+    reader = clean_major_fields(read_filled_csv())
+
+    def fil(entry):
+        
+        if "Mathematics" in entry['Major']:
+            return True
+        else:
+            return False
+
+    data = ImageData(reader, 
+                     "Major", 
+                     "Industry", 
+                     use_subvalues_left=True, 
+                     filter=fil)
+
+    conf = CircosConfig(data, 
+                        use_default_colors=True, 
+                        lside_tag_order=ordered_majors,
+                        rside_tag_order=ordered_industries)
+
+    conf.produce_image()
 
 def all_double_majors():
 
@@ -31,4 +55,25 @@ def all_double_majors():
     conf = CircosConfig(data, 
                         use_default_colors=False)
                      
+    conf.produce_image()
+
+def math_double_majors():
+
+    reader = clean_major_fields(read_filled_csv())
+
+    def fil(entry):
+        
+        if "Mathematics" in entry['Major']:
+            return True
+        else:
+            return False
+
+    data = ImageData(reader, 
+                     "Major", 
+                     "Industry", 
+                     filter=fil)
+
+    conf = CircosConfig(data, use_default_colors=True)
+                        
+
     conf.produce_image()
