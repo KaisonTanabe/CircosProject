b85df432f22f43a7acae8535d8f73f34fd8b03c6
Scott Sanderson <scoutoss@gmail.com>
2013-04-04 04:14:40 -0400

    added some more examples

--- example.py
+++ example.py
@@ -3,10 +3,32 @@ from config import *
 from projects.williams.double_major import *
 from data import ImageData
 
-if __name__ == "__main__":
+# Uses ordered ltags and rtags, default colors, left subvalues.
+def original_image():
     
     reader = clean_major_fields(read_filled_csv())
-    data = ImageData(reader, "Major", "Industry", use_subvalues_left=True)
+    data = ImageData(reader, 
+                     "Major", 
+                     "Industry", 
+                     use_subvalues_left=True)
 
-    conf = CircosConfig(data, use_default_colors=True)
+    conf = CircosConfig(data, 
+                        use_default_colors=True, 
+                        lside_tag_order=ordered_majors,
+                        rside_tag_order=ordered_industries)
+
+    conf.produce_image()
+
+
+def all_double_majors():
+
+    reader = clean_major_fields(read_filled_csv())
+
+    data = ImageData(reader, 
+                     "Major", 
+                     "Industry")
+
+    conf = CircosConfig(data, 
+                        use_default_colors=False)
+                     
     conf.produce_image()
